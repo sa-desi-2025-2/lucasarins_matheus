3941ddd4db44894c3bff49099e0507f301986fd9
MTHEUS JOSE VILICZINSKI <mtheus_viliczinski@estudante.sesisenai.org.br>
2025-10-28 15:48:55 -0300

    Class LogAuditorias Feita

--- maintence/admin.py
+++ maintence/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import Categoriaativos, Ativos, Alertas,Usuarios
+from .models import Categoriaativos, Ativos, Alertas,Usuarios,LogAuditorias
+
 
 admin.site.register(Categoriaativos)
 admin.site.register(Ativos)
 admin.site.register(Alertas)
 admin.site.register(Usuarios)
+admin.site.register(LogAuditorias)

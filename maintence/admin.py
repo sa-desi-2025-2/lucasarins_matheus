0e7c9b860c7e480789bcd8bd7bba7622e3601a30
MTHEUS JOSE VILICZINSKI <mtheus_viliczinski@estudante.sesisenai.org.br>
2025-10-28 15:40:37 -0300

    Class Usuarios Feita

--- maintence/admin.py
+++ maintence/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Categoriaativos, Ativos, Alertas
+from .models import Categoriaativos, Ativos, Alertas,Usuarios
 
 admin.site.register(Categoriaativos)
 admin.site.register(Ativos)
 admin.site.register(Alertas)
+admin.site.register(Usuarios)
--- maintence/models.py
+++ maintence/models.py
@@ -51,3 +51,18 @@ class Alertas(models.Model):
         db_table = 'alertas'
         verbose_name = 'Alerta'
 
+class Usuarios(models.Model):
+    id_usuario = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    senha_hash = models.CharField(max_length=255)
+    perfil_acesso = models.CharField(max_length=50)
+    re = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+         return f" {self.nome} - {self.id_usuario}"
+    
+    class Meta:
+        managed = False
+        db_table = 'usuarios'
+        verbose_name = 'Usuário'

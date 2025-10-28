90f6a08534a68f3e5a04ef09eaeb954d45e282f6
MTHEUS JOSE VILICZINSKI <mtheus_viliczinski@estudante.sesisenai.org.br>
2025-10-28 15:54:51 -0300

    Class Fornecedores Feita

--- maintence/admin.py
+++ maintence/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Categoriaativos, Ativos, Alertas,Usuarios,LogAuditorias
+from .models import Categoriaativos, Ativos, Alertas,Usuarios,LogAuditorias, Fornecedores
 
 
 admin.site.register(Categoriaativos)
@@ -8,3 +8,4 @@ admin.site.register(Ativos)
 admin.site.register(Alertas)
 admin.site.register(Usuarios)
 admin.site.register(LogAuditorias)
+admin.site.register(Fornecedores)
--- maintence/models.py
+++ maintence/models.py
@@ -83,3 +83,17 @@ class LogAuditorias(models.Model):
         managed = False
         db_table = 'logauditorias'
         verbose_name = 'Log de Auditoria'
+
+class Fornecedores(models.Model):
+    id_fornecedor = models.AutoField(primary_key=True)
+    nome_empresa = models.CharField(max_length=100)
+    cnpj = models.CharField(max_length=20, unique=True)
+    telefone = models.CharField(max_length=20)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.nome_empresa
+    class Meta:
+        managed = False
+        db_table = 'fornecedores'
+        verbose_name = 'Fornecedor'

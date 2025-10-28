c293fd7a299eff1eae7c82c7cc5dc6d816ff5d13
MTHEUS JOSE VILICZINSKI <mtheus_viliczinski@estudante.sesisenai.org.br>
2025-10-28 16:35:08 -0300

    Criado Class Reparos e Itens reparos

--- maintence/admin.py
+++ maintence/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Categoriaativos, Ativos, Alertas,Usuarios,LogAuditorias, Fornecedores
+from .models import Categoriaativos, Ativos, Alertas,Usuarios,LogAuditorias, Fornecedores, Itens, Reparos, ItensReparo
+
 
 
 admin.site.register(Categoriaativos)
@@ -9,3 +10,6 @@ admin.site.register(Alertas)
 admin.site.register(Usuarios)
 admin.site.register(LogAuditorias)
 admin.site.register(Fornecedores)
+admin.site.register(Itens)
+admin.site.register(Reparos)
+admin.site.register(ItensReparo)
--- maintence/models.py
+++ maintence/models.py
@@ -96,4 +96,57 @@ class Fornecedores(models.Model):
     class Meta:
         managed = False
         db_table = 'fornecedores'
-        verbose_name = 'Fornecedor'
+        verbose_name = 'Fornecedore'
+
+class Itens(models.Model):
+    id_item = models.AutoField(primary_key=True)
+    nome_peca = models.CharField(max_length=100)
+    quantidade = models.IntegerField()
+    custo_unitario = models.DecimalField(max_digits=10, decimal_places=2)
+    id_fornecedor = models.ForeignKey('Fornecedores', db_column='id_fornecedor', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome_peca
+    class Meta:
+        managed = False
+        db_table = 'itens'
+        verbose_name = 'Iten'
+
+
+class Reparos(models.Model):    
+    id_reparo = models.AutoField(primary_key=True)
+    id_ativo = models.ForeignKey('Ativos', db_column='id_ativo', on_delete=models.CASCADE)
+    data_reparo = models.DateTimeField(auto_now_add=True)
+    tipo = models.CharField(max_length=100)
+    descricao = models.TextField()
+    tempo_parada_hora = models.IntegerField()
+    extensao_vida_util = models.IntegerField()
+    unid_extensao_vida_util = models.CharField(max_length=20)
+    id_usuario = models.ForeignKey('Usuarios', db_column='id_usuario', on_delete=models.CASCADE)
+    roi_calculado = models.DecimalField(max_digits=10, decimal_places=2)
+    custo_total_peca = models.DecimalField(max_digits=12, decimal_places=2)
+    anexos = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return f"Reparo {self.id_reparo} para {self.id_ativo.nome}"
+
+    class Meta:
+        managed = False
+        db_table = 'reparos'
+        verbose_name = 'Reparo'
+
+
+
+class ItensReparo(models.Model):
+    id_itemreparo = models.AutoField(primary_key=True)
+    id_item = models.ForeignKey('Itens', db_column='id_item', on_delete=models.CASCADE)
+    id_reparo = models.ForeignKey('Reparos', db_column='id_reparo', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"ItemReparo {self.id_itemreparo}"
+    
+    class Meta:
+        managed = False
+        db_table = 'itensreparo'
+        verbose_name = 'Item de Reparo'
+
